0e07f3cb93007d86ba5217375dd5d36c9084f4cb
kingjames500 <jimmymaina500@gmail.com>
2024-09-19 15:24:18 +0300

    adding delete button, new event listener on the delete button

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ loadTasksFromLocalStorage();
 
 // let's create a function to add a task
 
-function addTask(){
+function addTask() {
     const task = taskInput.value.trim();
 
     if (task === '') {
         alert('Please enter a task');
         return;
     }
-    else{
+    else {
 
         createTaskElement(task);
 
@@ -30,20 +30,34 @@ function addTask(){
 
 //let's create a function to create the task
 
-function createTaskElement(task){
+function createTaskElement(task) {
     const listItem = document.createElement('li');
 
     listItem.textContent = task;
 
-    taskList.appendChild(listItem); 
+    //create a delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'delete';
+    deleteButton.className = 'delete-btn';
+
+    //append the delete button to the list item
+    listItem.appendChild(deleteButton);
+    taskList.appendChild(listItem);
+
+    //add event listener to the delete button
+
+    deleteButton.addEventListener('click', function () {
+        taskList.removeChild(listItem);
+        saveTaskToLocal();
+    });    
 }
 
 //function for saving task onto local storage
 
-function saveTaskToLocal(){
+function saveTaskToLocal() {
     let tasks = [];
-    taskList.querySelectorAll('li').forEach(function( item){
-        tasks.push(item.textContent.trim());
+    taskList.querySelectorAll('li').forEach(function (item) {
+        tasks.push(item.textContent.replace('delete', '').trim());
     });
 
     localStorage.setItem('tasks', JSON.stringify(tasks));
@@ -51,7 +65,7 @@ function saveTaskToLocal(){
 
 //function for loading task from local storage so that even if the page is refreshed the task will still be there 
 
-function loadTasksFromLocalStorage(){
+function loadTasksFromLocalStorage() {
     const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
     tasks.forEach(createTaskElement);
 }
